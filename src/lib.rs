d103084ed5627c7ac05e4a97b2048163e38e172f
Kevin Vuong <kevinvuong2023@gmail.com>
2022-09-29 12:56:21 -0500

    increased functionality and keys.txt

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,37 @@
 use std::{collections::HashMap, fs::{File, self}, io::{self, BufRead, BufReader, Error}, path::Path, ops::Deref};
+use serde_json::Value;
 use serpapi_search_rust::serp_api_search::SerpApiSearch;
 
 #[derive(Debug)]
-struct Dispenser {
+pub struct Dispenser {
     key: String,
     queries: Vec<String>
 }
 
 impl Dispenser {
-    fn new(key: String) -> Dispenser {
-
+    ///generates a new Dispenser object,
+    pub fn new(key_file: &str, queries_file: &str) -> Dispenser {
         Dispenser {
-            key: key,
-            queries: parse_text_file().unwrap()
+            key: parse_text_file(key_file).unwrap().join(""),
+            queries: parse_text_file(queries_file).unwrap()
         }
     }
-    /// Return the organic results from the search
-    fn get_oresults(search: SerpApiSearch) {
-    
+    /// Returns the organic results from a SerpApiSearch
+    async fn get_oresults(search: SerpApiSearch) -> Result<Value, Box<dyn std::error::Error>> {
+        let results = search.json().await?;
+
+        Ok(results["organic_results"].clone())
     }
     
-    pub fn search(&self, amt_queries_sent: Option<i32>) -> Result<(), Error>{
+    pub async fn tribe_search(&self, amt_queries_sent: Option<i32>) -> Result<(), Box<dyn std::error::Error>>{
         for query in 0..amt_queries_sent.unwrap_or(self.queries.len() as i32) {
             let mut params = HashMap::<String, String>::new();
             params.insert("engine".to_string(), "google".to_string());
             params.insert("q".to_string(), query.to_string());
 
-            let search = SerpApiSearch::google(params, self.key);
-            Self::get_oresults(search)
+            let search = SerpApiSearch::google(params, (*self.key).to_string());
+            Self::get_oresults(search).await;
         }
-
 
         Ok(())
     }
@@ -37,9 +39,9 @@ impl Dispenser {
 
 }
 
-fn parse_text_file() -> Result<Vec<String>, Error> {
+fn parse_text_file(name: &str) -> Result<Vec<String>, Error> {
     let mut tribes_list = Vec::new();
-    let contents = File::open("tribes.txt")?;
+    let contents = File::open(name)?;
     let reader = BufReader::new(contents);
 
     for line in reader.lines().filter(|x| x.as_deref().unwrap().len() > 0) {
@@ -61,14 +63,20 @@ mod tests {
 
     #[test]
     fn parse_text_file_test() {
-        let tribes_list = parse_text_file();
+        let tribes_list = parse_text_file("tribes.txt");
         println!("{:?}", tribes_list.unwrap());
         assert_eq!("2", "1");
     }
     #[test]
+    fn return_key() {
+        let key_list = parse_text_file("key.txt");
+        let key = &key_list.unwrap()[0];
+        assert_eq!("1", "2")
+    }
+    #[test]
     fn create_Dispenser () {
         let key = String::from("I'm a random key!");
-        let dispenser = Dispenser::new(key);
+        let dispenser = Dispenser::new("key.txt", "tribes.txt");
         
         println!("{:?}", dispenser);
         assert_eq!("1", "2");
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 
 use serpapi_search_rust::serp_api_search::SerpApiSearch;
 use std::collections::HashMap;
+use Native_American_Webscraping_Project::Dispenser;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let dispenser = Dispenser::new("key.txt", "tribes.txt");
 
-
-  let search = SerpApiSearch::google(params, "secret_api_key".to_string());
+  /*let search = SerpApiSearch::google(params, "secret_api_key".to_string());
   
   let results = search.json().await?;
   let organic_results = results["organic_results"];
@@ -16,6 +17,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("--- JSON ---");
   println!(" - results:", organic_results);
 
-  print!("ok");
+  print!("ok"); */
   Ok(())
 }
